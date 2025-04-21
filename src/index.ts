80d200596cfad334e839f217a0c2f63f6f47d41f
jhordyess <dev@jhordyess.com>
2025-04-21 19:41:01 -0400

    Refactor index.ts to improve route handling and error management

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,37 @@ import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
 
-import { port } from './config'
+import { API_BASE_PATH, PORT } from './config'
+
+import { errorHandler, healthCheckHandler, notFoundHandler } from './middlewares/handlers'
+import { openApiRouter } from './routes/openApi'
+import { apiRouter } from './routes/v1/router'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
+// Homepage
 app.get('/', (_, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// Health check
+app.get('/health', healthCheckHandler)
+
+// Routes
+app.use(`/docs`, openApiRouter)
+app.use(API_BASE_PATH, apiRouter)
+
+// Error handler
+app.use(errorHandler)
+
+// Not found handler
+app.use(notFoundHandler)
+
+app.listen(PORT, () => {
+  console.info(`Listening at http://localhost:${PORT}`)
 })
